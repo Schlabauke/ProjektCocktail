6e84b0333f2e7a1bb5561762296077825d98a79b
Schlabauke <annabelle.jeffre@gmail.com>
2022-01-03 19:14:27 +0100

    Update One

---
+++ src/components/CocktailTemp.js
@@ -0,0 +1,10 @@
+const CocktailTemp = () => {
+    return (
+        <article>
+            <h3>CocktailName</h3>
+            <img src="https://images.unsplash.com/photo-1583898350903-99fa829dad3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNvY2t0YWlsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="Cocktail" />
+        </article>
+    );
+}
+
+export default CocktailTemp;
