bed67f5e7a9a75699490415b904043c10af91798
StefanSeliger <Your_seligerstefan84@gmail.com>
2022-01-03 19:13:31 +0100

    third

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import Header from './components/Header.js';
-import AlcoholItem from './components/AlcoholItem.js';
-
-
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <AlcoholItem/>
+
     </div>
   );
 }
