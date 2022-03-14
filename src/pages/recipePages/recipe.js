ae98d3475460d1a2073a58e44402475ee35455f0
StefanSeliger <Your_seligerstefan84@gmail.com>
2022-03-14 16:30:02 +0100

    styles

--- src/pages/recipePages/recipe.js
+++ src/pages/recipePages/recipe.js
@@ -15,18 +15,23 @@ class cocktailDetails extends React.Component {
     render() {
         return (
             <section className="recipe">
-                <h2>{this.state.recipe.strDrink}</h2>
-                <img src={this.state.recipe.strDrinkThumb} alt={this.state.recipe.strDrink} />
-                <h3>Cocktail Rezept</h3>
-                <h4>Zutaten</h4>
-                <ul>
-                    <li>{this.state.recipe.strMeasure1} {this.state.recipe.strIngredient1}</li>
-                    <li>{this.state.recipe.strMeasure2} {this.state.recipe.strIngredient2}</li>
-                    <li>{this.state.recipe.strMeasure3} {this.state.recipe.strIngredient3}</li>
-                    <li>{this.state.recipe.strMeasure4} {this.state.recipe.strIngredient4}</li>
-                </ul>
-                <p>{this.state.recipe.strInstructionsDE}</p>
-                <BackButton />
+                <article className="recipeHeadline">
+                    <h2>{this.state.recipe.strDrink}</h2>
+                    <div>
+                    <img src={this.state.recipe.strDrinkThumb} alt={this.state.recipe.strDrink} />
+                    <h3>Cocktail Rezept</h3>
+                    <h4>Zutaten</h4>
+                    <ul>
+                        <li>{this.state.recipe.strMeasure1} {this.state.recipe.strIngredient1}</li>
+                        <li>{this.state.recipe.strMeasure2} {this.state.recipe.strIngredient2}</li>
+                        <li>{this.state.recipe.strMeasure3} {this.state.recipe.strIngredient3}</li>
+                        <li>{this.state.recipe.strMeasure4} {this.state.recipe.strIngredient4}</li>
+                    </ul>
+                    <p>{this.state.recipe.strInstructionsDE}</p>
+                    <BackButton />
+                    </div>
+                </article>
+                
             </section>
         );
     }
